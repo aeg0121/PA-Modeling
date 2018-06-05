d0fe7ba544c11ec496a5b0db05d5aaff45ad022f
Chance <tarver.chance@gmail.com>
2018-06-05 10:28:49 -0500

    Fix table append issue.

--- PA_Tables.m
+++ PA_Tables.m
@@ -1,6 +1,7 @@
 classdef PA_Tables
     %PA_Tables Class to contain all the tables.
-    %   Detailed explanation goes here
+    %   Construct the tables if they don't exist. If they do exist, then
+    %   load them from memory and append the new models to them.
     
     properties
         tables
@@ -11,21 +12,14 @@ classdef PA_Tables
             %PA_Tables Construct an instance of this class
             %   Detailed explanation goes here
             
+            load_flag = 0; % Inital case for load flag.
             [MAX_NONLINEAR_ORDER, MAX_MEMORY_ORDER] =  size(pa_models);
             try
                 %load a table if it exists
                 load('evaluate_pa_models');
+                load_flag = 1; % load was succesful
                 
-                %Should try to get this out of the try statement that way
-                %we can catch errors happening here.
-                for i = 1:2:MAX_NONLINEAR_ORDER
-                    for j = 1:MAX_MEMORY_ORDER
-                        index = sprintf('order%d%d',i,j);
-                        obj.tables.(index) =  obj.pa_model_table_append(pa_models(i,j), obj.tables.(index));
-                    end
-                end
             catch
-                %pa_model_11_table = pa_model_table_append(pa_model_11, pa_model_11_table);
                 for i = 1:2:MAX_NONLINEAR_ORDER
                     for j = 1:MAX_MEMORY_ORDER
                         index = sprintf('order%d%d',i,j);
@@ -33,8 +27,17 @@ classdef PA_Tables
                         obj = obj.pa_model_table(pa_models(i,j), i, j);
                     end
                 end
-                
             end
+            
+            if load_flag
+                for i = 1:2:MAX_NONLINEAR_ORDER
+                    for j = 1:MAX_MEMORY_ORDER
+                        index = sprintf('order%d%d',i,j);
+                        obj.tables.(index) =  obj.pa_model_table_append(pa_models(i,j), obj.tables.(index));
+                    end
+                end
+            end
+            
             save('evaluate_pa_models','obj');
             
         end
@@ -52,14 +55,21 @@ classdef PA_Tables
         
         function table = pa_model_table_append(obj, model, table)
             if size(table,1) ~= 1
-                table = table(1:end-5,:);
+                table = table(1:end-5, :);
             end
-            new_row = table(1,:);
+            
+            % make prototypes for new rows.
+            new_row = table(end,:);
+            [number_of_rows, ~] = size(table);
+            new_row_index = number_of_rows + 1;
+            
             med = table(1,:);
             absolute_average = table(1,:);
             sd = table(1,:);
             prct5 = table(1,:);
             prct95 = table(1,:);
+            
+            % Put the correct values in the new rows.
             for x = 1:length(model.PolyCoeffs)
                 new_row(1,x) = {model.PolyCoeffs(x)};
                 med(1,x) = {median([abs(table.(x)); abs(new_row.(x))])};
@@ -68,13 +78,14 @@ classdef PA_Tables
                 prct5(1,x) = {prctile([abs(table.(x)); abs(new_row.(x))], 5)};
                 prct95(1,x) = {prctile([abs(table.(x)); abs(new_row.(x))], 95)};
             end
+            
             new_row(1,x+1) = {model.mse_of_fit};
             med(1,x+1) = {median([table.(x+1);new_row.(x+1)])};
             absolute_average(1,x+1) = {mean([table.(x+1);new_row.(x+1)])};
             sd(1,x+1) = {std([abs(table.(x+1));abs(new_row.(x+1))])};
             prct5(1,x+1) = {prctile([abs(table.(x+1)); abs(new_row.(x+1))], 5)};
             prct95(1,x+1) = {prctile([abs(table.(x+1)); abs(new_row.(x+1))], 95)};
-            new_row.Properties.RowNames(1) = {num2str(32)};
+            new_row.Properties.RowNames(1) = {sprintf('Row%d', new_row_index)};
             med.Properties.RowNames(1) = {'Median'};
             absolute_average.Properties.RowNames(1) = {'Absolute Average'};
             sd.Properties.RowNames(1) = {'Standard Deviation'};
